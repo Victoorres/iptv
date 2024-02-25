a38a77a36f88be24a9eeffdf52130e13fdb897ef
Victor <victoorres@icloud.com>
2024-02-24 22:10:29 -0300

    feat: :sparkles: adding temporary home screen

--- src/app/initial-page/initial-page.component.ts
+++ src/app/initial-page/initial-page.component.ts
@@ -1,12 +1,14 @@
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-initial-page',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatToolbarModule, CommonModule, MatGridListModule],
   templateUrl: './initial-page.component.html',
-  styleUrl: './initial-page.component.scss'
+  styleUrl: './initial-page.component.scss',
 })
-export class InitialPageComponent {
-
-}
+export class InitialPageComponent {}
